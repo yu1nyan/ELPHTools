42226c2ffedddf006cf378c4637465fa4e401c15
yu1nyan <yuiyuiawayui@gmail.com>
2018-12-21 15:40:11 +0900

    Fix fit parameters for calibration in 9 cubes

--- makeCalib9Cubes.sh
+++ makeCalib9Cubes.sh
@@ -5,5 +5,5 @@ export sub=5
 export nfile=$2
 
 ./run_calib hodo $run $sub $nfile 20 70 830 830 png
-./run_calib proto1s $run $sub $nfile 20 70 830 830 png
-./run_calib proto2s $run $sub $nfile 20 70 855 840 png
+./run_calib proto1s $run $sub $nfile 20 70 825 825 png
+./run_calib proto2s $run $sub $nfile 20 70 845 830 png
