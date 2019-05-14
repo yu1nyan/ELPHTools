147c476667a35b08fe8a576ac7435d05482e4b61
yuinyan <yuiyuiawayui@gmail.com>
2019-05-14 20:45:14 +0900

    make drawEvtDisplay function & fix the options of drawCubeLine function

--- run2cubesAll.sh
+++ run2cubesAll.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 # non-welding cubes
-./run_2cubes 0 2 0 0 1 0
-./run_2cubes 3 8
+# ./run_2cubes 0 2 0 0 1 0
+# ./run_2cubes 3 8
+
+./run_2cubes 3 2 0 0 -1 0
+./run_2cubes 6 3 0 0 -1 0
 
 # w/ 100um sheet
-./run_2cubes 11 10
+# ./run_2cubes 11 10 0 0 -1 0
+./run_2cubes 12 1 0 0 -1 0
+./run_2cubes 19 1 0 0 -1 0
 
 # w/ 200um sheet
-./run_2cubes 22 10
+# ./run_2cubes 22 10
+./run_2cubes 24 1 0 0 -1 0
+./run_2cubes 27 4 0 0 -1 0
