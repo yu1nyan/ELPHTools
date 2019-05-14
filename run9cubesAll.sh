4078d662afaaa887d06212139b57f7a43db17e62
yuinyan <yuiyuiawayui@gmail.com>
2019-05-14 16:43:20 +0900

    small fix

--- run9cubesAll.sh
+++ run9cubesAll.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 # extruded cubes
-./run_9cubes 0 2
-./run_9cubes 4 10
-./run_9cubes 14 4
+./run_9cubes 0 1
+./run_9cubes 5 2
+./run_9cubes 8 2
+./run_9cubes 11 3
+./run_9cubes 15 2
 
 # injected cubes
-./run_9cubes 18 10
+./run_9cubes 19 2
+./run_9cubes 22 1
+./run_9cubes 26 2
+
+# All (including gap)
+# ./run_9cubes 0 2
+# ./run_9cubes 4 10
+# ./run_9cubes 14 4
+# ./run_9cubes 18 10
